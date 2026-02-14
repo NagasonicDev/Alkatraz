27ed7e2feab3236fe1ed969b5bfa929c29196f8a
NagasonicDev <153145285+NagasonicDev@users.noreply.github.com>
2026-02-14 20:59:23 +1100

    Implement Fireball Spell Options

--- core/src/main/java/me/nagasonic/alkatraz/spells/implementation/Fireball.java
+++ core/src/main/java/me/nagasonic/alkatraz/spells/implementation/Fireball.java
@@ -1,18 +1,19 @@
 package me.nagasonic.alkatraz.spells.implementation;
 
 import de.tr7zw.nbtapi.NBT;
-import de.tr7zw.nbtapi.iface.ReadableNBT;
 import me.nagasonic.alkatraz.Alkatraz;
 import me.nagasonic.alkatraz.config.ConfigManager;
 import me.nagasonic.alkatraz.config.Configs;
 import me.nagasonic.alkatraz.events.PlayerSpellPrepareEvent;
-import me.nagasonic.alkatraz.playerdata.DataManager;
-import me.nagasonic.alkatraz.playerdata.PlayerData;
-import me.nagasonic.alkatraz.spells.Spell;
 import me.nagasonic.alkatraz.spells.components.SpellComponent;
 import me.nagasonic.alkatraz.spells.components.SpellComponentHandler;
 import me.nagasonic.alkatraz.spells.components.SpellComponentType;
 import me.nagasonic.alkatraz.spells.components.SpellEntityComponent;
+import me.nagasonic.alkatraz.spells.configuration.OptionValue;
+import me.nagasonic.alkatraz.spells.configuration.SpellOption;
+import me.nagasonic.alkatraz.spells.configuration.impact.implementation.ManaCostImpact;
+import me.nagasonic.alkatraz.spells.configuration.impact.implementation.StatModifierImpact;
+import me.nagasonic.alkatraz.spells.configuration.requirement.implementation.NumberStatRequirement;
 import me.nagasonic.alkatraz.spells.types.AttackSpell;
 import me.nagasonic.alkatraz.spells.types.AttackType;
 import me.nagasonic.alkatraz.spells.types.BarrierSpell;
@@ -20,16 +21,14 @@ import me.nagasonic.alkatraz.spells.types.properties.implementation.AttackProper
 import me.nagasonic.alkatraz.util.ParticleUtils;
 import me.nagasonic.alkatraz.util.Utils;
 import org.bukkit.*;
-import org.bukkit.block.data.type.Fire;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import java.util.List;
@@ -38,6 +37,89 @@ import java.util.UUID;
 public class Fireball extends AttackSpell implements Listener {
     public Fireball(String type) {
         super(type);
+    }
+
+    @Override
+    protected void setupOptions(){
+        SpellOption speedOption = new SpellOption(this, "speed",
+                "Adjust fireball velocity", Material.FEATHER);
+        OptionValue<Double> slowSpeed = new OptionValue<>(
+                "slow", "Slow", "Slower projectile, maximum damage",
+                Material.SOUL_SAND, 0.3
+        );
+        slowSpeed.addImpact(new StatModifierImpact(this, "projectile_speed", 0.3, StatModifierImpact.ModifierType.SET));
+        slowSpeed.addImpact(new StatModifierImpact(this, "damage", 1.2, StatModifierImpact.ModifierType.MULTIPLY));
+        speedOption.addValue(slowSpeed);
+
+        OptionValue<Double> normalSpeed = new OptionValue<>(
+                "normal", "Normal", "Standard projectile speed",
+                Material.FEATHER, 0.5
+        );
+        normalSpeed.addImpact(new StatModifierImpact(this, "projectile_speed", 0.5, StatModifierImpact.ModifierType.SET));
+        normalSpeed.addImpact(new StatModifierImpact(this, "damage", 1.0, StatModifierImpact.ModifierType.MULTIPLY));
+        speedOption.addValue(normalSpeed);
+
+        OptionValue<Double> fastSpeed = new OptionValue<>(
+                "fast", "Fast", "Faster projectile, reduced damage (-20%)",
+                Material.SUGAR, 0.8
+        );
+        fastSpeed.addRequirement(new NumberStatRequirement<>("circleLevel", 2, "Requires Circle Level 2"));
+        fastSpeed.addImpact(new StatModifierImpact(this, "projectile_speed", 0.8, StatModifierImpact.ModifierType.SET));
+        fastSpeed.addImpact(new StatModifierImpact(this, "damage", 0.8, StatModifierImpact.ModifierType.MULTIPLY));
+        speedOption.addValue(fastSpeed);
+
+        OptionValue<Double> blazingSpeed = new OptionValue<>(
+                "blazing", "Blazing", "Extremely fast, heavy damage penalty (-40%)",
+                Material.BLAZE_POWDER, 1.2
+        );
+        blazingSpeed.addRequirement(new NumberStatRequirement<>("circleLevel", 4, "Requires Circle Level 4"));
+        blazingSpeed.addImpact(new StatModifierImpact(this, "projectile_speed", 1.2, StatModifierImpact.ModifierType.SET));
+        blazingSpeed.addImpact(new StatModifierImpact(this, "damage", 0.6, StatModifierImpact.ModifierType.MULTIPLY));
+        blazingSpeed.addImpact(new ManaCostImpact(this, 15));
+        speedOption.addValue(blazingSpeed);
+
+        addOption(speedOption);
+
+        SpellOption breaksBlocksOption = new SpellOption(this, "breaks_blocks",
+                "Whether the fireball should break blocks", Material.DIAMOND_PICKAXE);
+        OptionValue<Boolean> breaksBlocks = new OptionValue<>(
+                "breaks_blocks", "Breaks Blocks", "Fireball will break blocks on collision", Material.LIME_CONCRETE, true
+        );
+        breaksBlocksOption.addValue(breaksBlocks);
+        OptionValue<Boolean> notBreakBlocks = new OptionValue<>(
+                "block_protection", "Blocks Protected", "Fireball will not break blocks.", Material.RED_CONCRETE, false
+        );
+        breaksBlocksOption.addValue(notBreakBlocks);
+
+        addOption(breaksBlocksOption);
+
+        SpellOption sizeOption = new SpellOption(this, "size",
+                "Adjust fireball explosion size", Material.TNT);
+        OptionValue<Double> small = new  OptionValue<>(
+                "small_size", "Small Size", "Small fireball explosion.", Material.IRON_NUGGET, 0.75
+        );
+        small.addRequirement(new NumberStatRequirement<>("circleLevel", 3, "Requires Circle Level 3"));
+        small.addImpact(new StatModifierImpact(this, "size", 0.75,  StatModifierImpact.ModifierType.SET));
+        small.addImpact(new StatModifierImpact(this, "damage", 1.4, StatModifierImpact.ModifierType.SET));
+        small.addImpact(new StatModifierImpact(this, "projectile_speed", 1.1, StatModifierImpact.ModifierType.MULTIPLY));
+        sizeOption.addValue(small);
+
+        OptionValue<Double> normal = new OptionValue<>(
+                "normal_size", "Normal Size", "Normal fireball explosion", Material.IRON_INGOT, 1.0
+        );
+        normal.addImpact(new StatModifierImpact(this, "size", 1, StatModifierImpact.ModifierType.SET));
+        sizeOption.addValue(normal);
+
+        OptionValue<Double> large = new OptionValue<>(
+                "large_size", "Large Size", "Large fireball explosion", Material.IRON_BLOCK, 1.25
+        );
+        large.addRequirement(new NumberStatRequirement<>("circleLevel", 3, "Requires Circle Level 3"));
+        large.addImpact(new StatModifierImpact(this, "size", 1.25, StatModifierImpact.ModifierType.SET));
+        large.addImpact(new StatModifierImpact(this, "damage", 0.6, StatModifierImpact.ModifierType.MULTIPLY));
+        large.addImpact(new StatModifierImpact(this, "projectile_speed", 0.9, StatModifierImpact.ModifierType.SET));
+        sizeOption.addValue(large);
+
+        addOption(sizeOption);
     }
 
     @Override
@@ -63,21 +145,22 @@ public class Fireball extends AttackSpell implements Listener {
     public void castAction(Player caster, ItemStack wand) {
         if (caster.isDead()) return;
 
-        // Create per-cast AttackProperties
-        AttackProperties props = new AttackProperties(caster, Utils.castLocation(caster), getBasePower() * NBT.get(wand, nbt -> (Double) nbt.getDouble("magic_power")), AttackType.MAGIC);
+        double projectileSpeed = getModifiedStat(caster, "projectile_speed", 0.5);
 
-        // Launch the fireball
-        org.bukkit.entity.Fireball fireball = caster.launchProjectile(org.bukkit.entity.Fireball.class, caster.getEyeLocation().getDirection().multiply(0.5));
-
-
-        // Wrap the fireball in a SpellEntityComponent and register
-        SpellEntityComponent entityComp = new SpellEntityComponent(
-                this,
-                props,
+        AttackProperties props = new AttackProperties(
                 caster,
-                wand,
-                SpellComponentType.OFFENSE,
-                fireball
+                Utils.castLocation(caster),
+                getBasePower() * NBT.get(wand, nbt -> (Double) nbt.getDouble("magic_power")),
+                AttackType.MAGIC
+        );
+
+        org.bukkit.entity.Fireball fireball = caster.launchProjectile(
+                org.bukkit.entity.Fireball.class,
+                caster.getEyeLocation().getDirection().multiply(projectileSpeed)
+        );
+
+        SpellEntityComponent entityComp = new SpellEntityComponent(
+                this, props, caster, wand, SpellComponentType.OFFENSE, fireball
         );
         entityComp.setCollisionRadius(0.5);
         NBT.modifyPersistentData(fireball, nbt -> {
@@ -119,6 +202,24 @@ public class Fireball extends AttackSpell implements Listener {
         SpellComponent comp = SpellComponentHandler.getActiveComponent(UUID.fromString(idString));
         if (!(comp instanceof SpellEntityComponent ecomp)) return;
         if (!(ecomp.getProperties() instanceof AttackProperties props)) return;
-        e.setDamage(getPower(comp.getCaster(), le, props.getRemainingPower()));
+        double finalDamage = getPower(comp.getCaster(), le, props.getRemainingPower());
+        e.setDamage(finalDamage);
+
+    }
+
+    @EventHandler
+    private void onFireballExplode(EntityExplodeEvent e) {
+        if (!(e.getEntity() instanceof org.bukkit.entity.Fireball fireball)) return;
+        String idString = NBT.getPersistentData(fireball, nbt -> nbt.getString("component_id"));
+        if (idString == null) return;
+        SpellComponent comp = SpellComponentHandler.getActiveComponent(UUID.fromString(idString));
+        if (comp.getSpell().getClass() != Fireball.class) return;
+        if (!(comp instanceof SpellEntityComponent ecomp)) return;
+        if (!(ecomp.getProperties() instanceof AttackProperties props)) return;
+        if ((boolean) getOption("breaks_blocks").getSelectedValue(comp.getCaster()).getValue()) return;
+        e.setCancelled(true);
+        for (LivingEntity entity : fireball.getLocation().getNearbyLivingEntities((double) getOption("size").getSelectedValue(comp.getCaster()).getValue())){
+            entity.damage(getPower(comp.getCaster(), entity, props.getRemainingPower()));
+        }
     }
 }
