25f969a7adc3e025b27644bb53a2194aa742fe9b
NagasonicDev <153145285+NagasonicDev@users.noreply.github.com>
2026-07-23 18:50:46 +1000

    fix: add PlaceholderAPI dependency and PluginHook base class

--- core/src/main/java/me/nagasonic/alkatraz/hooks/PluginHook.java
+++ core/src/main/java/me/nagasonic/alkatraz/hooks/PluginHook.java
@@ -1,4 +1,23 @@
 package me.nagasonic.alkatraz.hooks;
 
-public class PluginHook {
+import me.nagasonic.alkatraz.Alkatraz;
+
+public abstract class PluginHook {
+    protected final String pluginName;
+    protected final boolean present;
+
+    public PluginHook(String pluginName) {
+        this.present = Alkatraz.getInstance().getServer().getPluginManager().getPlugin(pluginName) != null;
+        this.pluginName = pluginName;
+    }
+
+    public String getPluginName() {
+        return pluginName;
+    }
+
+    public boolean isPresent() {
+        return present;
+    }
+
+    public abstract void ifPresent();
 }
